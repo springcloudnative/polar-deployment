520f14de5a9af5cf57ecd3be017044847ab90304
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-04-01 16:24:21 +0200

    Updating docker compose file

---
+++ docker/mysql/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE polardb_catalog;
+CREATE USER 'catalog_user'@'%' IDENTIFIED BY 'Y2F0YWxvZ191c2VyCg==';
+CREATE USER 'order_user'@'%' IDENTIFIED BY 'b3JkZXJfdXNlcgo=';
+GRANT CREATE on polardb_catalog.* TO 'catalog_user'@'%';
+GRANT CREATE on polardb_catalog.* TO 'order_user'@'%';
+GRANT ALL PRIVILEGES ON polardb_catalog.books TO 'catalog_user'@'%';
+GRANT ALL PRIVILEGES ON polardb_catalog.orders TO 'order_user'@'%';
+GRANT ALL PRIVILEGES ON polardb_catalog.flyway_schema_history TO 'catalog_user'@'%';
+GRANT ALL PRIVILEGES ON polardb_catalog.flyway_schema_history TO 'order_user'@'%';
