228e59fa0337941881913412c89836119f199083
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-04-06 17:39:09 +0200

    Updating init script

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -5,5 +5,5 @@ GRANT CREATE on polardb_catalog.* TO 'catalog_user'@'%';
 GRANT CREATE on polardb_catalog.* TO 'order_user'@'%';
 GRANT ALL PRIVILEGES ON polardb_catalog.books TO 'catalog_user'@'%';
 GRANT ALL PRIVILEGES ON polardb_catalog.orders TO 'order_user'@'%';
-GRANT ALL PRIVILEGES ON polardb_catalog.flyway_schema_history TO 'catalog_user'@'%';
-GRANT ALL PRIVILEGES ON polardb_catalog.flyway_schema_history TO 'order_user'@'%';
+GRANT ALL PRIVILEGES ON polardb_catalog.catalog__flyway_schema_histor TO 'catalog_user'@'%';
+GRANT ALL PRIVILEGES ON polardb_catalog.order__flyway_schema_histor TO 'order_user'@'%';
